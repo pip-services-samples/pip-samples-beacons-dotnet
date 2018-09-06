c01f6d9a3e7a5c1b87d1fcda39ecb4e31a4b2108
Алексей Двойкин <alexeydvoykin@MacBook-Pro-Aleksej.local>
2018-09-06 18:43:05 +0300

    [all steps] updated all, fixed problems

---
+++ step5/src/Service/Services/Version1/BeaconsHttpServiceV1.cs
@@ -0,0 +1,14 @@
+﻿using PipServices.Commons.Refer;
+using PipServices.Rpc.Services;
+
+namespace Beacons.Services.Version1
+{
+    public class BeaconsHttpServiceV1: CommandableHttpService
+    {
+        public BeaconsHttpServiceV1()
+            : base("v1/beacons")
+        {
+            _dependencyResolver.Put("controller", new Descriptor("beacons", "controller", "default", "*", "1.0"));
+        }
+    }
+}
